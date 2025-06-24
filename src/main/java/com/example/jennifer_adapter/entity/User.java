4a735793a93734ec19292f3827df50b9d83aeead
Radies <ranyoung0102@gmail.com>
2025-06-24 10:03:58 +0900

    frist commit

---
+++ src/main/java/com/example/jennifer_adapter/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.jennifer_adapter.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private LocalDate birthDate;
+}
